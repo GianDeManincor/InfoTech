2ab7aec8169f2ec565ed8d0378eac2ce03c66d8f
Gian <gianmanincor@gmail.com>
2020-09-09 22:42:03 -0300

    Criei a classe Cliente e organizei a estrutura do projeto

---
+++ src/main/java/com/mycompany/infotech/models/Cliente.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.infotech.models;
+
+/**
+ *
+ * @author Gian
+ */
+public class Cliente {
+    
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String sexo;
+    private String email;
+    
+    public Cliente(){
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }   
+    
+}
