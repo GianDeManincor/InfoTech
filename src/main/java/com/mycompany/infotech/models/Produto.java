7132bdb5dfbfb0b0c039b051bd4ed4bc60a4a066
Gian <gianmanincor@gmail.com>
2020-09-22 22:57:54 -0300

    Criei a classe Produto

---
+++ src/main/java/com/mycompany/infotech/models/Produto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.infotech.models;
+
+/**
+ *
+ * @author Gian
+ */
+//Classe Produto
+public class Produto {
+    
+}
